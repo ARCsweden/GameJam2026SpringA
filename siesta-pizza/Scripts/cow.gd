4c1f53f3e92acc3c6451bb8f9809fa406100e82e
Erik <31732187+eln17011@users.noreply.github.com>
2026-05-09 14:11:08 +0200

    Cow

--- siesta-pizza/Scripts/cow.gd
+++ siesta-pizza/Scripts/cow.gd
@@ -2,5 +2,5 @@ extends InteractableObject
 
 
 # Called when the node enters the scene tree for the first time.
-func interact(player):
+func interact_extra(player):
 	pass # Replace with function body.
