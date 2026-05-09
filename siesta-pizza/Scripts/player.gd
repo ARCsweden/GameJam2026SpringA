5a6b69de380ed9680de24b030b10503ad87a1707
Sandflakes <harbl4x@gmail.com>
2026-05-09 13:28:53 +0200

    Animation speed scaling + origin correction

--- siesta-pizza/Scripts/player.gd
+++ siesta-pizza/Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 200
+@export var speed = 90
 
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
@@ -14,16 +14,25 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _process(delta):
-	if abs(velocity.x) > abs(velocity.y):
-		$AnimatedSprite2D.animation = "walk_right"
-		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-		
-	elif abs(velocity.y) > abs(velocity.x):
-		if velocity.y < 0:
-			$AnimatedSprite2D.animation = "walk_up"
-		else:
-			$AnimatedSprite2D.animation = "walk_down"
 	
+	
+# Animation controller	
+	if(velocity.length() > 0):
+		$AnimatedSprite2D.speed_scale = velocity.length()/15;
+		if abs(velocity.x) >= abs(velocity.y):
+			$AnimatedSprite2D.animation = "walk_right"
+			$AnimatedSprite2D.flip_v = false
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+			
+		elif abs(velocity.y) > abs(velocity.x):
+			$AnimatedSprite2D.flip_h = false
+			if velocity.y < 0:
+				$AnimatedSprite2D.animation = "walk_up"
+			else:
+				$AnimatedSprite2D.animation = "walk_down"
 	else:
-		$AnimatedSprite2D.animation = "idle_down"
+		$AnimatedSprite2D.speed_scale = 1.0;
+		if(Input.is_action_pressed("Action")):
+			$AnimatedSprite2D.animation = "tbag"
+		else:
+			$AnimatedSprite2D.animation = "idle_down"
