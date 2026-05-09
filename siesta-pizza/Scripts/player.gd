42d563a8be8a7206df2025992d2854a8887ea8a2
Sandflakes <harbl4x@gmail.com>
2026-05-09 11:48:30 +0200

    Added animations

--- siesta-pizza/Scripts/player.gd
+++ siesta-pizza/Scripts/player.gd
@@ -1,11 +1,29 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 200
 
 func get_input():
 	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
 	velocity = input_direction * speed
 
+func _ready():
+	$AnimatedSprite2D.play()
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+func _process(delta):
+	if abs(velocity.x) > abs(velocity.y):
+		$AnimatedSprite2D.animation = "walk_right"
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		
+	elif abs(velocity.y) > abs(velocity.x):
+		if velocity.y < 0:
+			$AnimatedSprite2D.animation = "walk_up"
+		else:
+			$AnimatedSprite2D.animation = "walk_down"
+	
+	else:
+		$AnimatedSprite2D.animation = "idle_down"
