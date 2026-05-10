c0960dc933a7f54d78280c0136bc8cba7c3eff14
Erik <31732187+eln17011@users.noreply.github.com>
2026-05-10 13:29:35 +0200

    Pizza

---
+++ siesta-pizza/Scripts/pizza_combinator.gd
@@ -0,0 +1,23 @@
+extends InteractableObject
+
+var tomato_sauce_count = 0
+var cheese_count = 0
+var dough_count = 0
+
+# Called when the node enters the scene tree for the first time.
+func interact_extra(player, item):
+	if item != null:
+		if item.item_name == "tomato":
+			tomato_sauce_count = tomato_sauce_count + 1
+		if item.item_name == "dough":
+			dough_count = dough_count + 1
+		if item.item_name == "milk":
+			cheese_count = cheese_count + 1
+		player.remove_item()
+		if ((dough_count > 0) && (tomato_sauce_count > 1) && (cheese_count > 1)):
+			_give_item(player, storage_out)
+			tomato_sauce_count = tomato_sauce_count - 1
+			dough_count = dough_count - 1
+			cheese_count = cheese_count - 1
+	print_debug("Pizza combinator interacted")
+	
